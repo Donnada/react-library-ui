7ce4f8e0def58195a1011cde02dafc1bedea464b
docata Li <814331154@qq.com>
2024-01-24 21:14:09 +0800

    Initial commit

---
+++ rollup.config.mjs
@@ -0,0 +1,34 @@
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
+import dts from "rollup-plugin-dts";
+import packagejson from "./package.json" assert { type: "json" };
+
+// const packagejson = require("./package.json");
+export default [
+    {
+        input:"src/index.ts",
+        output:[
+            {
+                file:packagejson.main,
+                format:"cjs",
+                sourcemap:true,
+            },
+            {
+                file:packagejson.module,
+                format:"esm",
+                sourcemap:true,
+            },
+        ],
+        plugins:[
+            resolve(),
+            commonjs(),
+            typescript({tsconfig:"./tsconfig.json"}),
+        ]
+    },
+    {
+        input:"dist/esm/types/index.d.ts",
+        output:[{file:"dist/index.d.ts",format:"esm"}],
+        plugins:[dts()],
+    }
+]
